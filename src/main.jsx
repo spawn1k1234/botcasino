a5553bfef9cb71394fb014c76cbe260ec350baa7
spawn1k1234 <vladislav.sidorov2008@gmail.com>
2025-06-23 19:42:19 +0200

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 ReactDOM.render(
-  <TonConnectUIProvider manifestUrl="https://botcasino.vercel.app//tonconnect-manifest.json">
+  <TonConnectUIProvider manifestUrl="https://botcasino.vercel.app/tonconnect-manifest.json">
     <App />
   </TonConnectUIProvider>,
   document.getElementById("root")
