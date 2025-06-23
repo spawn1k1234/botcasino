345248a30eb88fbbfae6fb7b789a61e92678e4da
spawn1k1234 <vladislav.sidorov2008@gmail.com>
2025-06-23 17:22:24 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "./", // для относительных путей
+  root: ".", // указывает на корень проекта
+  publicDir: "public", // указывает папку с публичными файлами
 });
