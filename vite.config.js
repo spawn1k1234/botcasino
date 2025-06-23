ec5a4455ad368eb12ae2ba427e5e0a957b63f601
spawn1k1234 <vladislav.sidorov2008@gmail.com>
2025-06-23 17:36:11 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // относительный путь для Vercel
-  publicDir: "public", // указываем папку для статических файлов
-  build: {
-    outDir: "dist", // указываем, куда будет собираться проект
-  },
 });
